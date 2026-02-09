59dd1de84b4d12fd9ca9718997ce1a2b0d3d1bb4
Arpit <arpitkharche123@gmail.com>
2026-02-09 18:44:04 +0530

    some minor changes

--- api-gateway/src/main/java/edu/j2ee/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/edu/j2ee/api_gateway/ApiGatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+//This will also be registered in service registry//This will also be registered in service registry
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
